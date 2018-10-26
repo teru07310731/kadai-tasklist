65e4b5255cf645f00b3c0bb6e4d48a17f7b4aefb
teruteru07310731 <40971412+teru07310731@users.noreply.github.com>
2018-10-26 20:04:21 +0000

    9

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
-  get 'signup', to: 'users#new'
 
+
+  get 'signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tasks
+  resources :tasks, only: [:index, :show, :new, :create]
 end
