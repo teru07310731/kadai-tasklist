48b365352a63ecbb49cf9105ddb0694722f1460b
teruteru07310731 <40971412+teru07310731@users.noreply.github.com>
2018-10-28 05:23:11 +0000

    12

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessinos#index'
   post 'login', to: 'sessinos#create'
+  delete 'logout', to: 'sessinos#destroy'
 
   get 'signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
