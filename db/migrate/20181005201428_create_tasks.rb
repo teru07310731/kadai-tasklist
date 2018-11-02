e8fcadf15a0f6082d375fdf700b26f81d0c0d41e
teruteru07310731 <40971412+teru07310731@users.noreply.github.com>
2018-11-02 04:42:15 +0000

    21

--- db/migrate/20181005201428_create_tasks.rb
+++ db/migrate/20181005201428_create_tasks.rb
@@ -2,7 +2,6 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :content
-      t.references :user, forein_key: true
 
       t.timestamps
     end
